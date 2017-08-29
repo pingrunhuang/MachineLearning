c51436ceb790cdef83b2405dc526432adceb57c6
Frank <pingrunhuang@gmail.com>
2017-08-29 14:01:54 +0800

    not converge yet:(

--- linear_regression/README.md
+++ linear_regression/README.md
@@ -9,12 +9,12 @@ Our goal is to get the appropriate theta(0) and theta(1) so the the value of J c
 # Gradient descent
 
 * should simultaneous update theta(0) and theta(1)
-temp_theta0 = theta0 - alpha * np.diff(J(theta0, theta1), theta0) = theta0 - alpha * 1/2 * 1/m * sum(h(x(i) - y(i)))
-temp_theta1 = theta1 - alpha * np.diff(J(theta0, theta1), theta1) = theta1 - alpha * 1/2 * 1/m * sum(h(x(i) - y(i))) * x(i)
+temp_theta0 = theta0 - alpha * np.diff(J(theta0, theta1), theta0) = theta0 - alpha * 1/m * sum(h(x(i)) - y(i))
+temp_theta1 = theta1 - alpha * np.diff(J(theta0, theta1), theta1) = theta1 - alpha * 1/m * sum((h(x(i)) - y(i)) * x(i))
 theta0=temp_theta0
 theta1=temp_theta1
 
 * turns out that the gradient decent will always get the global optimal.
 
 * different kind of gradient descent:
-  * Batch gradient descent: 
+  * Batch gradient descent:
--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -2,6 +2,7 @@
 import pickle
 import pandas as pd
 import numpy as np
+import sympy
 from sklearn.model_selection import train_test_split
 
 '''
@@ -27,17 +28,49 @@ test_ratio = 0.3
 train_dataset, test_dataset = train_test_split(df, test_size=test_ratio)
 
 trainX = train_dataset.iloc[:,:13]
-trainY = train_dataset.iloc[:,13]
+trainY = train_dataset.iloc[:,13] * 1000
 testX = test_dataset.iloc[:,:13]
-testY = test_dataset.iloc[:,13]
-
+testY = test_dataset.iloc[:,13] * 1000
 MSE = 10
-theta0 = np.random.rand(1,1)
-theta1 = np.random.rand(1,13)
+theta0 = np.random.rand(1)
+theta1 = np.random.rand(trainX.iloc[0].count())
+count = 0
+alpha = 0.001
 
-# y = theta0 + theta1 * x
-print(trainX.multiply(theta1,axis=0))
+# y_estimate = trainX.dot(theta1.transpose()) + theta0
+# print(y_estimate)
+# # update simultaneously
+# # this is equivilant to theta0 - alpha * 1/m * sum(h(x(i)) - y(i))
+# temp_theta0 = theta0 - alpha * np.sum(y_estimate - trainY) / trainY.count()
+# # this is equivilant to theta1 - alpha * 1/m * sum((h(x(i)) - y(i)) * x(i))
+# # I use * here instead of .dot because each column is multiplying the same value which is the corresponding h(x) - y
+# temp_theta1 = theta1 - alpha * ((y_estimate - trainY) * trainX.transpose()).sum(axis=1)/trainY.count()
+# theta0 = temp_theta0
+# theta1 = temp_theta1
+# # update MSE: 1/2 * 1/m * sum(square(h(x(i)) - y(i)))
+# MSE = np.sum(np.square(theta0 + trainX.dot(theta1)-trainY)) / ( 2 * trainY.count())
+# print('h(x):' + str(theta0 + trainX.dot(theta1)))
+# print('y:' + str(trainY))
+# print('MSE:' + str(MSE))
 
 
-# while MSE > 0.5:
-#     temp_theta0 = theta0 - trainX
+
+
+while MSE > 0.5:
+    # h(x) = theta0 + theta1 * x
+    y_estimate = trainX.dot(theta1.transpose()) + theta0
+    # update simultaneously
+    # this is equivilant to theta0 - alpha * 1/m * sum(h(x(i)) - y(i))
+    temp_theta0 = theta0 - alpha * np.sum(y_estimate - trainY) / trainY.count()
+    # this is equivilant to theta1 - alpha * 1/m * sum((h(x(i)) - y(i)) * x(i))
+    # I use * here instead of .dot because each column is multiplying the same value which is the corresponding h(x) - y
+    temp_theta1 = theta1 - alpha * ((y_estimate - trainY) * trainX.transpose()).sum(axis=1)/trainY.count()
+    theta0 = temp_theta0
+    theta1 = temp_theta1
+    # update MSE: 1/2 * 1/m * sum(square(h(x(i)) - y(i)))
+    MSE = np.sum(np.square(theta0 + trainX.dot(theta1)-trainY)) / ( 2 * trainY.count())
+    print('How many iteration: ' + str(count))
+    # print('h(x):' + str(theta0 + trainX.dot(theta1)))
+    # print('y:' + str(trainY))
+    print(MSE)
+    count = count + 1
